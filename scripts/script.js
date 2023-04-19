255ac8f16eb7a55879ed515f979b88e59a35d912
JajaiDG <130600250+JajaiDG@users.noreply.github.com>
2023-04-19 17:17:02 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -30,10 +30,11 @@ function recalculate() {
     let costLabel = document.getElementById("calculated-cost");
     let totalCost = 0;
     if (modelName === "XYZ") {
-    totalCost = duration * 100;
-    } else if (modelName === "CPRG") {
-    totalCost = duration * 213;
-    }
+        totalCost = duration * 100;
+        } 
+    else if (modelName === "CPRG") {
+        totalCost = duration * 213;
+        }
     costLabel.innerHTML = totalCost.toFixed(2);
     }
 
@@ -57,11 +58,12 @@ let modelButton = document.getElementById("model-button");
 function changeModel() {
     let modelText = document.getElementById("model-text");
     if (modelName === "XYZ") {
-    modelName = "CPRG";
-    modelText.innerHTML = "Model CPRG";
-    } else if (modelName === "CPRG") {
-    modelName = "XYZ";
-    modelText.innerHTML = "Model XYZ";
+        modelName = "CPRG";
+        modelText.innerHTML = "Model CPRG";
+        } 
+    else if (modelName === "CPRG") {
+        modelName = "XYZ";
+        modelText.innerHTML = "Model XYZ";
     }
     recalculate();
     }
